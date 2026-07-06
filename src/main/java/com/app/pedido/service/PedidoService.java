e60b47264aca5f8ddee147114e8eaf9aac14f5e2
Vitória Devigili <vitoriadevigili@gmail.com>
2026-07-05 22:20:36 -0300

    remove import sem uso

--- src/main/java/com/app/pedido/service/PedidoService.java
+++ src/main/java/com/app/pedido/service/PedidoService.java
@@ -19,7 +19,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
